5080fe98244fb728db01dcd576537fc65a0dd92c
echolocate <markdance@gmail.com>
2021-11-26 13:39:02 +0000

    added database uri to env

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -4,4 +4,7 @@ echo "Deploy stage"
 scp docker-compose.yaml jenkins@ci-swarm-manager:/home/jenkins/docker-compose.yaml
 ssh jenkins@ci-swarm-manager \
     DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR \
+    DATABASE_URI=$DATABASE_URI \
+    CREATE_SCHEMA=true \
     docker stack deploy --compose-file docker-compose.yaml todo-app
+
