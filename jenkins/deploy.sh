ff88b0dfe76e0c545741f05569833394ebd69e81
echolocate <markdance@gmail.com>
2021-11-26 11:46:42 +0000

    deploy stage copies compose file to manager

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -2,4 +2,5 @@
 
 echo "Deploy stage"
 
+scp docker-compose.yaml jenkins@ci-swarm-manager:/home/jenkins/docker-compose.yaml
 ssh jenkins@ci-swarm-manager docker stack deploy --compose-file docker-compose.yaml todo-app
