5323a9bc28fc31f0f4e02699850f8d33f440a2c6
echolocate <markdance@gmail.com>
2021-11-26 09:23:31 +0000

    progress maybe

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -9,7 +9,7 @@ sudo apt-get install -y curl jq
 # install docker
 curl https://get.docker.com | sudo bash
 sudo usermod -aG docker jenkins
-newgrp docker
+
 
 #install docker-compose
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
